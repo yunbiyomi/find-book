f2b9e091eb74faa106c00cc595a8bb353c43d7f3
yunbiyomi <yunbiyomi@naver.com>
2023-11-15 16:57:18 +0900

    💄 Global Style 지정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import BookDetailPages from "./pages/BookDetailPages";
+import { Global, css } from "@emotion/react";
 
 const Layout = () => {
   return (
     <>
+      <Global 
+        styles={css`
+          body {
+            background-color: #F2F4F3;
+            color: #0A0908;
+            transition-duration: .2s;
+            transition-property: background-color, color;
+          }
+          a {
+            color: #0A0908;
+            text-decoration: none;
+          }
+          ul {
+            list-style: none;
+            padding: 0;
+          }
+        `}
+      />
       <Outlet />
     </>
   )
