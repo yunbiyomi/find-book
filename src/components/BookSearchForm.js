7354fc3bd2c92b1601e0e4b4808b0a1b024cd3a8
yunbiyomi <yunbiyomi@naver.com>
2023-11-15 19:37:57 +0900

    💄 SearchPage 컴포넌트 스타일

---
+++ src/components/BookSearchForm.js
@@ -0,0 +1,47 @@
+import styled from '@emotion/styled';
+import React from 'react'
+
+const BookSearchForm = () => {
+  return (
+    <Form>
+      <Input type='search' placeholder='책 이름을 검색해주세요.'/>
+      <Button>Searcg</Button>
+    </Form>
+  )
+}
+
+export default BookSearchForm
+
+const Form = styled.form``;
+
+const Input = styled.input`
+  outline: none;
+  padding: .6rem 1rem;
+  border: 1px solid #5E503F;
+  border: 3px;
+  min-width: 280px;
+
+  &:focus,
+  &:active {
+    border-color: #221F1B;
+  }
+
+  @media (max-width: 778px) {
+    margin-top: 10px;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #5E503F;
+  color: #F2F4F3;
+  padding: 1rem;
+  margin-left: 15px;
+  border-radius: 3px;
+  outline: none;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: #DED6CE;
+    color: #221F1B;
+  }
+`;
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import styled from '@emotion/styled'
+import React, { useState } from 'react'
+import BookSearchForm from '../components/BookSearchForm'
 
 const SearchPage = () => {
   return (
-    <div>SearchPage</div>
+    <>
+      <Header>
+        <HeaderContainer>
+          <LogoText>FIND BOOK</LogoText>
+          <HeaderSearchForm>
+            <BookSearchForm />
+          </HeaderSearchForm>
+        </HeaderContainer>
+      </Header>
+    </>
   )
 }
 
 export default SearchPage
+
+const Header = styled.header`
+  border-bottom: 1px solid #221F1B;
+`;
+
+const Container = styled.div`
+  max-width: 960px;
+  padding: 15px;
+  margin: 0 auto;
+`;
+
+const HeaderContainer = styled(Container)`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 778px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`;
+
+const LogoText = styled.h1`
+  margin: 0;
+`;
+
+const HeaderSearchForm = styled.div`
+  margin-left: auto;
+`;
