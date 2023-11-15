5a2e65bc3fbcba12ea1377d138d2d2c4694a6a51
yunbiyomi <yunbiyomi@naver.com>
2023-11-15 16:47:31 +0900

    ✨ Route 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+import BookDetailPages from "./pages/BookDetailPages";
+
+const Layout = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  )
+}
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<SearchPage />} />
+          <Route path="/book/:bookId" element={<BookDetailPages />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
